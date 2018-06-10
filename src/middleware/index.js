6e943bd532bd503f42a5348e3b3ee537628ef70f
爆頭專家 <tom19960222@gmail.com>
2018-06-11 01:47:22 +0800

    Add recommand data

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -2,4 +2,33 @@
 module.exports = function (app) {
   // Add your custom middleware here. Remember, that
   // in Express the order matters
+  app.get('/recommand', (req, res) => {
+    res.status(200).json([
+      {
+        href: 'https://exhentai.org/g/1140340/8eac4a7bc1/',
+        img: 'https://exhentai.org/t/19/92/199200de3af7694131520a033318d8e370693c15-937559-1591-2270-jpg_l.jpg',
+        text: '[High-Spirit (あおぎりぺんた)] コスって!!トイレの花子くん (ハッカドール、艦隊これくしょん -艦これ-) [中国翻訳] [DL版]',
+      },
+      {
+        href: 'https://exhentai.org/g/1233128/ad22c56f40/',
+        img: 'https://exhentai.org/t/be/34/be340175036792b2221c59b7755e19989d998f77-316830-560-420-jpg_l.jpg',
+        text: '[ぐじら4号 (ぐじら)] お義父さんのオンナになったボク [中国翻訳]',
+      },
+      {
+        href: 'https://exhentai.org/g/1196236/f2bbdbcca6/',
+        img: 'https://exhentai.org/t/84/6e/846ea30a4db1af1252d3d48229ebad0446b9a44f-729655-1410-2024-jpg_l.jpg',
+        text: '[喪loss庵 (もろちんさん)] どきどきメスイキ!ちゃれんじ!!～コスプレ売り子編～ (ハッカドール、艦隊これくしょん -艦これ-) [中国翻訳] [DL版]',
+      },
+      {
+        href: 'https://exhentai.org/g/1211451/de49b9aaec/',
+        img: 'https://exhentai.org/t/22/e9/22e983a21a0bed4a700ccc37d199259488e445fd-885623-1636-2100-jpg_l.jpg',
+        text: '[カルムアトモスフィア (しんや)] 男の娘 Fuck Compilation [中国翻訳] [DL版]',
+      },
+      {
+        href: 'https://exhentai.org/g/1229720/c6687219f9/',
+        img: 'https://exhentai.org/t/d1/e9/d1e9086e1f75334f3652b3e4e12bfb89e85890ac-551045-1881-2700-jpg_l.jpg',
+        text: '[まりあな海溝企画 (みかみ北都)] キメセク島風くん (艦隊これくしょん -艦これ-) [中国翻訳] [DL版]',
+      },
+    ]);
+  });
 };
